fdfcb64482277d577383910d347690d784487dff
Konan Yao <yao.konan2709@gmail.com>
2025-02-28 11:48:13 +0100

    Fin de la navbar et header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { HeaderComponent } from "./header/header.component";
+import { NavComponent } from "./nav/nav.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TooltipModule, HeaderComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { importProvidersFrom } from '@angular/core';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), importProvidersFrom([TooltipModule.forRoot()])]
 };
