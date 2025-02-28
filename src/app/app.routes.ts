df8fdb26560ea403c8f60b375bafe9a66583b450
Konan Yao <yao.konan2709@gmail.com>
2025-02-28 12:03:54 +0100

    Ajout des liens dans la navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { CVComponent } from './cv/cv.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'acceuil', component: AcceuilComponent},
+    {path: 'portfolio', component: PortfolioComponent},
+    {path: 'cv', component: CVComponent},
+    {path: 'contact', component: ContactComponent},
+];
